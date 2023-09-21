9b7ec70ef05d6dde33c10b81e67b47bfd625da43
Martin Kribs <martin.kribs.mk@gmail.com>
2023-09-21 10:52:02 +0200

    init

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 # Einlesen der .csv-Datei  Auto mit Ausgabe der ersten 10 Zeilen
 import pandas as pd
 
-file = pd.read_csv("Auto.csv", sep=";", decimal=".", nrows=100)
-#print(file.head(10))
+file = pd.read_csv("Auto.csv", sep=";", decimal=",")
+print(file.head(10))
 
-#print(file.isnull().sum())
+# Abfrage Datentypen der Spalten
+print(file.dtypes)
 
-#file.dropna(how="all",inplace=True)
-#print(file.head(5))
+print(file["mpg"].iloc[0:7])
+
+print("Mittelwert der 1. Spalte", file["mpg"].mean().round(3))
+
+# NAN-Behandlung
+print(file.isnull().sum())
+
+# Löschen einer Zeile nur mit NAN
+file.dropna(how="any", inplace=True)
+
+print(file.head(5))
+print(file.isnull().sum())
+
+mw = file["mpg"].mean()
+print("Mittelwert:", mw)
+
+file["mpg"].fillna(mw, inplace=True)
+print(file["mpg"].iloc[30:40])
